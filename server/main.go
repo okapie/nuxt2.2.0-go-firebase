ce0c7f850b13dcf075b7b62b4766aa28f5d257be
okapie <tatigreuze@yahoo.co.jp>
2018-11-02 22:15:36 +0900

    Modify main.go for getting API response as a json.

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "encoding/json"
     "log"
     "net/http"
 
@@ -9,11 +10,11 @@ import (
 )
 
 func public(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("hello public!\n"))
+    json.NewEncoder(w).Encode(map[string]string{"message": "hello public!"})
 }
 
 func private(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("hello private!\n"))
+    json.NewEncoder(w).Encode(map[string]string{"message": "hello private!"})
 }
 
 func main() {
@@ -22,8 +23,8 @@ func main() {
     allowedHeaders := handlers.AllowedHeaders([]string{"Authorization"})
 
     r := mux.NewRouter()
-    r.HandleFunc("/public", public)
-    r.HandleFunc("/private", private)
+    r.HandleFunc("/public", public).Methods("GET")
+    r.HandleFunc("/private", private).Methods("GET")
 
     log.Fatal(http.ListenAndServe(":8000", handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(r)))
 }
